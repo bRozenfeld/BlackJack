222f1dc1720b105b06cb7d08d83cc9f736a873da
haita <haita@DESKTOP-H9IMPQK>
2020-03-03 11:21:53 +0100

    Implement player interface and playerImpl class

--- Common/src/main/java/fr/ensibs/player/Player.java
+++ Common/src/main/java/fr/ensibs/player/Player.java
@@ -1,4 +1,45 @@
 package fr.ensibs.player;
 
-public interface Player {
+import fr.ensibs.card.Card;
+
+import java.rmi.Remote;
+import java.util.List;
+
+public interface Player extends Remote {
+    /**
+     * Give the player name
+     * @return the player name
+     */
+    String getName();
+
+    /**
+     * Set player name
+     * @param name player name
+     */
+    void setName(String name);
+
+    /**
+     * Get the player score
+     * @return player score
+     */
+    Integer getScore();
+
+    /**
+     * Set the player score
+     * @param score player score
+     */
+    void setScore(Integer score);
+
+    /**
+     * Get the player list of cards
+     * @return list of player cards
+     */
+    List<Card> getCards();
+
+    /**
+     * Add a card to the player list of cards
+     * @param card card to add to the list of player cards
+     */
+    void AddCard(Card card);
+
 }
