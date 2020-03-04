ba98518899af68304b9b533a1ddd443097055f88
haita <haita@DESKTOP-H9IMPQK>
2020-03-04 09:25:14 +0100

    Implement playerApp class

---
+++ Common/src/main/java/fr/ensibs/player/Action.java
@@ -0,0 +1,10 @@
+package fr.ensibs.player;
+
+/**
+ * An enumeration of the action of a player
+ */
+public enum Action {
+    WAIT,
+    ADDCARD,
+    STOP
+}
--- Common/src/main/java/fr/ensibs/player/Player.java
+++ Common/src/main/java/fr/ensibs/player/Player.java
@@ -3,6 +3,7 @@ package fr.ensibs.player;
 import fr.ensibs.card.Card;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface Player extends Remote {
@@ -10,36 +11,43 @@ public interface Player extends Remote {
      * Give the player name
      * @return the player name
      */
-    String getName();
+    String getName() throws RemoteException;
 
     /**
      * Set player name
      * @param name player name
      */
-    void setName(String name);
+    void setName(String name) throws RemoteException;
 
     /**
      * Get the player score
      * @return player score
      */
-    Integer getScore();
+    Integer getScore() throws RemoteException;
 
     /**
      * Set the player score
      * @param score player score
      */
-    void setScore(Integer score);
+    void setScore(Integer score) throws RemoteException;
 
     /**
      * Get the player list of cards
      * @return list of player cards
      */
-    List<Card> getCards();
+    List<Card> getCards() throws RemoteException;
 
     /**
      * Add a card to the player list of cards
      * @param card card to add to the list of player cards
      */
-    void AddCard(Card card);
+    void addCard(Card card) throws RemoteException;
+    /**
+     * Give the player action
+     *
+     * @return the player action
+     */
+    Action getAction() throws RemoteException;
 
+    void setAction(Action action) throws RemoteException;
 }
