44149db1c9616818289e51c65a1e665d2439196e
haita <haita@DESKTOP-H9IMPQK>
2020-03-10 11:07:29 +0100

    IMplement chat methods

---
+++ Common/src/main/java/fr/ensibs/player/TextListener.java
@@ -0,0 +1,27 @@
+package fr.ensibs.player;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
+
+public class TextListener implements MessageListener {
+    private Player player;
+    public TextListener(Player player){
+        this.player=player;
+    }
+
+    @Override
+    public void onMessage(Message m) {
+        try{
+            if(m instanceof TextMessage){
+                TextMessage tMessage = (TextMessage) m;
+                System.out.println(player.getName() +": "+tMessage.getText());
+            }else{
+                System.out.println("Message is not an ObjectMessage ");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
